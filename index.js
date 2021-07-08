e23892d522db05adf910d09845130f203af41285
Artem Morhunov <a.morhunov@smissltd.com>
2021-07-08 05:19:33 +0300

    sdsfd

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todo')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout:'main',
+    extname:'hbs'
+})
+
+app.engine('hbs',hbs.engine)
+app.set('view engine','hbs')
+app.set('views','views')
+
+app.use(todoRoutes)
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://artem:starwars9@cluster0.xuydq.mongodb.net/todo',{
+            useNewUrlParser:true,
+            useFindAndModify:false,
+        })
+        app.listen(PORT,()=>{
+            console.log('server started')
+        })
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+start()
+
---
+++ routes/todo.js
@@ -0,0 +1,13 @@
+const {Router} = require('express')
+
+const router = Router()
+
+router.get('/',(req,res)=>{
+    res.render('index')
+})
+
+router.get('/create',(req,res)=>{
+    res.render('create')
+})
+
+module.exports = router
